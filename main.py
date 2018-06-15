6482b701f9e5f7c994cad04431fdc789a09ba677
Charles Copley <charles@praekelt.org>
2018-06-15 09:21:27 +0200

    added a POST method to the htpp

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, abort, request 
+import json
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
   return 'Hello, World!'
+
+@app.route('/resource', methods = ['POST'])
+def update_text():
+  print(request.json)
+  return json.dumps(request.json)
 
 if __name__ == '__main__':
   app.run()
