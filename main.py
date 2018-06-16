2962c0df5d6581b4b8b85478a43a0081aaaab3f7
Charles Copley <charles@praekelt.org>
2018-06-16 20:57:44 +0200

    completed refactor to class based code

--- main.py
+++ main.py
@@ -11,60 +11,6 @@ def home_screen():
     return 'Hmmmm!'
 
 @app.route('/getProducts', methods = ['POST'])
-def get_product():
-    """
-    End point to return the products associated with a phone number
-    """
-    json_data = request.get_json()
-    apikey = json_data['apikey']
-    apisecret = json_data['apisecret']
-    login = json_data['login']
-    url_login = json_data['url_login']
-    token = json_data['token']
-    phone = json_data['phone']
-    value = json_data['products_val']
-    simulate = json_data['simulate']
-    products = get_msisdn_products(phone, 
-                                   json_data, 
-                                   apikey, 
-                                   apisecret)
-    print(products)
-    return(json.dumps(products))
-
-@app.route('/addData', methods = ['POST'])
-def update_text():
-    """
-    End point to actually load data onto a phone number
-    """
-    logging.basicConfig(filename='myapp.log', 
-                        level=logging.INFO)
-    logging.info('Started')
-    json_data = request.get_json()
-    apikey = json_data['apikey']
-    logging.info('%s ', apikey)
-    apisecret = json_data['apisecret']
-    login = json_data['login']
-    url_login = json_data['url_login']
-    token = json_data['token']
-    phone = json_data['phone']
-    value = json_data['products_val']
-    simulate = json_data['simulate']
-    products = get_msisdn_products(phone, 
-                                   json_data, 
-                                   apikey, 
-                                   apisecret)
-    product_id = get_product_id(products,
-                                value)
-    fixed_recharge = payload_generation(phone,
-                                        str(product_id),
-                                        simulate)
-    services = post_transferto_goods(apikey,
-                                    apisecret,
-                                    'https://api.transferto.com/v1.1/transactions/fixed_value_recharges',
-                                    fixed_recharge)
-    return(services.text)
-
-@app.route('/getProductsObject', methods = ['POST'])
 def getProductObject():
     """
     End point to return the products associated with a phone number
@@ -74,7 +20,7 @@ def getProductObject():
     products = tf.get_msisdn_products()
     return(json.dumps(products))
 
-@app.route('/addDataObject', methods = ['POST'])
+@app.route('/addData', methods = ['POST'])
 def addDataObject():
     """
     End point to actually load data onto a phone number
