0ffa914be42caa85153e659ebb6bd35eb574bfae
Charles Copley <charles@praekelt.org>
2018-06-23 20:27:04 +0200

    added integration between transferto and rapidpro

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def add_rapidpro_object():
     tf.get_product_id()
     tf.payload_generation()
     services = tf.post_transferto_goods('https://api.transferto.com/v1.1/transactions/fixed_value_recharges')
-    return(services.text)
+    return(json_data)
     #return(json.dumps(fields))
 
 if __name__ == '__main__': 
