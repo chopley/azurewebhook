58d795718d9bb3f746dc25f8f2eef2745a31ad70
Charles Copley <charles@praekelt.org>
2018-06-23 15:19:25 +0200

    added beginnings of tests

---
+++ test_credentials.py
@@ -0,0 +1,8 @@
+from azurewebhook_functions import *
+from flask import Flask, abort, request, logging 
+import logging
+import json
+
+
+tf = Transferto()
+tf.read_credentials_file("transfertocredentials.json")
