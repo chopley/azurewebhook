fa16d788558c1d04b01622734777c3559ee8ca0f
Charles Copley <charles@praekelt.org>
2018-06-25 14:28:02 +0200

    json.dumps as output

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def add_rapidpro_object():
     tf.payload_generation()
     services = tf.post_transferto_goods('https://api.transferto.com/v1.1/transactions/fixed_value_recharges')
     #return(services.text)
-    return(urllib.parse.urlencode(services.json()))
+    return(json.dumps(services.json()))
 
 if __name__ == '__main__': 
     app.run(host= '0.0.0.0')
